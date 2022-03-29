2150deaf1ce5c00cdd57d0b27fd6b7dd795ba554
Brooks Rosenberg <bjtr@bu.edu>
2022-03-29 13:11:45 -0400

    fixed server js to add send file index

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const app = express()
 const port = 3001
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
