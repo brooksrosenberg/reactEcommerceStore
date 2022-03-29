21e37a6e5789d179d845c0ee593d82fc5d9e0071
Brooks Rosenberg <bjtr@bu.edu>
2022-03-29 13:37:45 -0400

    added build to client and small edit to server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3001;
 
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
