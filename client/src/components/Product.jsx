f0c39010a593a25bd68c493adf1b1669e85f1d92
Shalah <zoungranaala@gmail.com>
2022-03-31 11:43:46 -0400

    Adding the description and other few components needed for a single page

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -2,9 +2,19 @@ import './product.css'
 import React from 'react'
 
 const Product = () => {
-  return (
-    <div>Product</div>
-  )
+  <article className='wrapper'>
+        <div className='imageContainer'>
+            <img className='image' src='https://images.pexels.com/photos/6984650/pexels-photo-6984650.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'/>
+        </div>
+        <div className='infoContainer'>
+            <span className='title'> The Model </span>
+            <p className='description'> Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                Quas esse quaerat nemo hic blanditiis. Minus eligendi odio quaerat fugit,
+                 sit deserunt voluptates amet, 
+                vero eveniet ducimus repudiandae nostrum. Doloremque, sequi?</p>
+            <span className='price'> $ 2000 </span>
+        </div>
+    </article>
 }
 
 export default Product
