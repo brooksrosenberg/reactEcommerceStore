0545cb1ca254683b05b582f41cbb23696ac84393
Will Neal <willwneal@gmail.com>
2022-03-31 15:27:21 -0400

    working on Product and Product list component to render from database

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -3,30 +3,9 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
 const Product = () => {
-  const [products, setProduct] = useState([]);
-  const getProducts = async (res, req) => {
-    try {
-      const response = await axios.get('/api/product');
-      console.log(response.data);
-      setProduct(response.data);
-      
-    } catch (err) {
-      res.status(500).json(err)
-    }
-  }
-
-  useEffect(()=>{
-    getProducts();
-  }, []);
-  
   return (
     <div>
-      <button onClick={getProducts}>
-        Get Products
-      </button>
-      <button onClick={()=> console.log(products)}>
-        Show Products
-      </button>
+      Product
     </div>
   )
 }
