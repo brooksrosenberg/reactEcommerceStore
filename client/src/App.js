12a641604d67096e2ae5cba9d591146d8d60cdf5
Brooks Rosenberg <bjtr@bu.edu>
2022-03-31 11:06:04 -0400

    fixing cart functionality

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,11 @@ import './App.css';
 import { Home } from './pages/Home'
 import Category from './components/Categories'
 import RegisterPlaceholder from './components/RegisterPlaceholder';
-// <<<<<<< shoppingcart
-// import SignIn from './components/SignIn';
-// import Pay from './components/Pay';
-// import Cart from './pages/Shoppingcart';
-// import Success from './components/Success';
+import SignIn from './components/SignIn';
+import Pay from './components/Pay';
+import Cart from './pages/Shoppingcart';
+import Success from './components/Success';
 
-// =======
-// // <<<<<<< login-page
-// // import SignIn from './components/SignIn';
-// // =======
-// // import Pay from './components/Pay'
-// // import Success from './components/Success';
-
-// // >>>>>>> main
-// >>>>>>> main
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import React from 'react';
 
@@ -38,34 +28,20 @@ function App() {
           element={<RegisterPlaceholder/>}
         />
         <Route
-// <<<<<<< shoppingcart
-//           path='/signin'
-//           element={<SignIn/>}
-//           />
-//           <Route
-//           path='/pay'
-//           element={<Pay/>}
-//         />
-//         <Route
-//           path='/success'
-//           element={<Success/>}
-//         />
-//         <Route
-//           path='/cart'
-//           element={<Cart/>}
-// =======
-// // <<<<<<< login-page
-// //           path='/signin'
-// //           element={<SignIn/>}
-// // =======
-// //           path='/pay'
-// //           element={<Pay/>}
-// //         />
-// //         <Route
-// //           path='/success'
-// //           element={<Success/>}
-// // >>>>>>> main
-// >>>>>>> main
+          path='/signin'
+          element={<SignIn/>}
+          />
+          <Route
+          path='/pay'
+          element={<Pay/>}
+        />
+        <Route
+          path='/success'
+          element={<Success/>}
+        />
+        <Route
+          path='/cart'
+          element={<Cart/>}
         />
       </Routes>
     </Router>
