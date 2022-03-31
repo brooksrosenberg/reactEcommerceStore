c1c89d9906621bbb2e82701ec1ecd59323438b02
Colin Mosi Cargile <cargile10@gmail.com>
2022-03-31 10:18:46 -0400

    created a register page and the css for the page

--- client/src/components/RegisterPlaceholder.jsx
+++ client/src/components/RegisterPlaceholder.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import './register.css'
+
 
 const RegisterPlaceholder = () => {
   return (
-    <div>Register</div>
+    <div className='register-body'>
+    <div className='register-container'>
+        <div className='register-title'>CREATE AN ACCOUNT</div>
+        <form action='#'></form>
+          <div className='register-user-details'>
+            <div className='input-box'>
+              <span className='details'>Username</span>
+              <input type='text' placeholder='Enter your username' required></input>
+            </div>
+            <div className='input-box'>
+              <span className='details'>Email</span>
+              <input type='email' placeholder='Enter your email' required></input>
+            </div>
+            <div className='input-box'>
+              <span className='details'>Password</span>
+              <input type='password' placeholder='Enter your password' required></input>
+            </div>
+            <div className='input-box'>
+              <span className='details'>Confirm Password</span>
+              <input type='password' placeholder='Confirm your password' required></input>
+            </div>
+          </div>
+          <div className='register-button'>
+            <input type='submit' value='Register'></input>
+          </div>
+    </div>
+    </div>
   )
 }
 
