39e8755fcd9621fe7b032d320d29a3383d3b56e0
Will Neal <willwneal@gmail.com>
2022-03-30 20:11:41 -0400

    Integrated Stripe and created Success/Pay Components. Added various new packages

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import { Home } from './pages/Home'
 import Category from './components/Categories'
 import RegisterPlaceholder from './components/RegisterPlaceholder';
+import Pay from './components/Pay'
+import Success from './components/Success';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import React from 'react';
 
@@ -20,6 +23,14 @@ function App() {
         <Route
           path='/register'
           element={<RegisterPlaceholder/>}
+        />
+        <Route
+          path='/pay'
+          element={<Pay/>}
+        />
+        <Route
+          path='/success'
+          element={<Success/>}
         />
       </Routes>
     </Router>
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,12 +4,13 @@ const productRoute = require('./product');
 const cartRoute = require('./cart');
 const orderRoute = require('./order')
 const authRoute = require('./auth')
+const stripeRoute = require('./stripe')
 
 router.use('/user', userRoute);
 router.use('/product', productRoute);
 router.use('/cart', cartRoute);
 router.use('/order', orderRoute);
 router.use('/auth', authRoute);
-
+router.use('/checkout', stripeRoute)
 
 module.exports = router;
