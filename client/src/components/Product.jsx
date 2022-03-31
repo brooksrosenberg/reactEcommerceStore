24a536485c05a878d55abd23d8b143d4fd574fad
Will Neal <willwneal@gmail.com>
2022-03-31 19:39:35 -0400

    Created product list component, added it to home page and cleaned the categoryItem.css

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -1,13 +1,28 @@
 import './product.css'
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+import { ShoppingCartOutlined, SearchOutlined, FavoriteBorderOutlined } from '@material-ui/icons'
 
-const Product = () => {
+const Product = ({ item }) => {
   return (
-    <div>
-      Product
-    </div>
-  )
+    <div className='p-container'>
+        <div className='p-description'>
+          <p className='p-text'>This is demonstration text</p>
+        </div>
+        <img src={item.img} className='p-image'></img>
+        <div className="p-info">
+          <div className="p-icon">
+            <ShoppingCartOutlined/>
+          </div>
+          <div className="p-icon">
+            <FavoriteBorderOutlined/>
+          </div>
+          <div className="p-icon">
+            <SearchOutlined/>
+          </div>
+        </div>
+        
+      </div>
+      )
 }
 
-export default Product
+      export default Product
