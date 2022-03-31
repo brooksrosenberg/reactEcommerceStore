261302ce47f8acda816b2c37fb7cf3357dd4e75f
Shalah <zoungranaala@gmail.com>
2022-03-31 12:57:24 -0400

    Changed the background and created a file to style the whole page

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -1,41 +1,48 @@
 import React from 'react'
+import Announcement from '../components/Announcement'
+import Navbar from '../components/Navbar'
 
 
 const Product = () => {
   return (
-    <article className='wrapper'>
-        <div className='imageContainer'>
-            <img className='image' src='https://images.pexels.com/photos/6984650/pexels-photo-6984650.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'/>
-        </div>
-        <div className='infoContainer'>
-            <span className='title'> The Model </span>
-            <p className='description'> Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                Quas esse quaerat nemo hic blanditiis. Minus eligendi odio quaerat fugit,
-                 sit deserunt voluptates amet, 
-                vero eveniet ducimus repudiandae nostrum. Doloremque, sequi?</p>
-            <span className='price'> $ 2000 </span>
-            <div className='filterContainer'>
-                <div>
-                    <details>Color</details>
-                    <span color='black'/>
-                    <span color='darkblue'/>
-                    <span color='gray'/>
-                </div>
-
-                <div>
-                    <details>Size</details>
+    <div>
+        <Navbar />
+        <Announcement />
+        <article className='wrapper'>
+            <div className='imageContainer'>
+                <img className='image' src='https://images.pexels.com/photos/1485031/pexels-photo-1485031.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'/>
+            </div>
+            <div className='infoContainer'>
+                <span className='title'> The Model </span>
+                <p className='description'> Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Quas esse quaerat nemo hic blanditiis. Minus eligendi odio quaerat fugit,
+                    sit deserunt voluptates amet, 
+                    vero eveniet ducimus repudiandae nostrum. Doloremque, sequi?</p>
+                <span className='price'> $ 2000 </span>
+                <div className='filterContainer'>
                     <div>
-                        <span> XS</span>
-                        <span>S</span>
-                        <span>M</span>
-                        <span>L</span>
-                        <span>XL</span>
+                        <details>Color</details>
+                        <span color='black'/>
+                        <span color='darkblue'/>
+                        <span color='gray'/>
                     </div>
-                    
+
+                    <div>
+                        <details>Size</details>
+                        <div>
+                            <span> XS</span>
+                            <span>S</span>
+                            <span>M</span>
+                            <span>L</span>
+                            <span>XL</span>
+                        </div>
+                        
+                    </div>
                 </div>
             </div>
-        </div>
-    </article>
+        </article>
+    </div>
+    
     
   )
 }
