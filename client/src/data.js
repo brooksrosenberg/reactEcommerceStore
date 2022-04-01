3746c8cf0821b5c64a4c46aac2772bdd9f97b113
Will Neal <willwneal@gmail.com>
2022-04-01 16:04:01 -0400

    got rid of icons and made the image a link

--- client/src/data.js
+++ client/src/data.js
@@ -1,17 +1,17 @@
 const categories =[
     {
         id: 1,
-        img: "https://images.pexels.com/photos/10660920/pexels-photo-10660920.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        img: "https://images.pexels.com/photos/3341231/pexels-photo-3341231.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         title: "Mens",
     },
     {
         id: 2,
-        img: "https://images.pexels.com/photos/1598505/pexels-photo-1598505.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", 
+        img: "https://images.pexels.com/photos/2048695/pexels-photo-2048695.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", 
         title: "Womens"
     },
     {
         id: 3,
-        img: "https://images.pexels.com/photos/6712027/pexels-photo-6712027.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        img: "https://images.pexels.com/photos/1540321/pexels-photo-1540321.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         title: "Accessories"
     }
 ]
