488a9563da4129f30c4e4b4cca0eefdcd28d566e
Brooks Rosenberg <bjtr@bu.edu>
2022-04-02 10:50:16 -0400

    fixing category setup and product data

--- client/src/components/SingleProduct.jsx
+++ client/src/components/SingleProduct.jsx
@@ -8,25 +8,27 @@ const SingleProduct = ({ item }) => {
     <div className='p-container'>
         <div className='p-description'>
           <h3 className='p-title'>{item.title}</h3>
-          <p className='p-text'>{item.description}</p>
+          {/* <p className='p-text'>{item.description}</p> */}
         </div>
         <Link to={`/product/${item._id}`}>
         <img src={item.img} className='p-image'></img>
         </Link>
-        {/* <div className="p-info">
+        {/* <div>Price: ${item.price}</div> */}
+        
+         {/* <div className="p-info">
           <div className="p-icon">
             <ShoppingCartOutlined/>
-          </div>
-          <div className="p-icon">
+          </div> */}
+          {/* <div className="p-icon">
             <FavoriteBorderOutlined/>
           </div>
           
           <div className="p-icon">
             <SearchOutlined/>
-          </div> */}
+          </div>  */}
           
-        </div>
-      // </div>
+        {/* </div> */}
+    </div>
       )
 }
       export default SingleProduct
