2610eba5d8548ac4d3253faad726913fdae7ba29
Shalah <zoungranaala@gmail.com>
2022-04-01 22:25:52 -0400

    Changing the overall style of the checkout page

--- client/src/pages/Shoppingcart.jsx
+++ client/src/pages/Shoppingcart.jsx
@@ -42,33 +42,38 @@ export default function Cart() {
     <div className="cart-container">
       <div className="cart-wrapper">
         <div className="cart-card-container">
-          <h1 className="cart-title">Your Bag</h1>
-          <p className="cart-description">View your items below</p>
+          <h1 className="cart-title">Shopping Bag</h1>
+          {/* <p className="cart-description">View your items below</p> */}
             <div className="cart-items">
                 <li className="cart-list">
                     <ul>{products.map((cart) => {
                       console.log(cart.title);
                     return (
                       <div key={cart}>
-                      <div>Title:{cart.title}</div>
-                      <div>Description:{cart.description}</div>
-                      <img className='cart-product-img' alt='cart product'src={cart.img}></img>
-                      <div>Category:{cart.category}</div>
-                      <div>Size:{cart.size}</div>
-                      <div>Price:{cart.price}</div>
-                      <div>Location:{cart.location}</div>
+                        <img className='cart-product-img' alt='cart product'src={cart.img}></img>
+                      <div> <span className="cart-details"> Title: </span> {cart.title}</div>
+                      <div> <span className="cart-details"> Description: </span> {cart.description}</div>
+                      <div> <span className="cart-details"> Category: </span> {cart.category}</div>
+                      <div> <span className="cart-details"> Size: </span> {cart.size}</div>
+                      <div> <span className="cart-details"> Price: $</span> {cart.price}</div>
+                      <div> <span className="cart-details"> Location: </span> {cart.location}</div>
+                      <br></br>
+                      <br></br>
                       </div>
-                    
                     )})}
                     </ul>
                 </li>
             </div>
-            <div className='checkout-button'>
-              <input onClick={submitForm} type='button' value='Checkout'></input>
+
+            <div className="cart-buttons">
+              <div className='checkout-button'>
+                <input onClick={submitForm} type='button' value='Checkout'></input>
+              </div>
+              <div className='Go-Back-button'>
+                <input onClick={gobackForm} type='button' value='Go Back'></input>
+              </div>
             </div>
-            <div className='Go-Back-button'>
-              <input onClick={gobackForm} type='button' value='Go Back'></input>
-            </div>
+           
         </div>
       </div>
     </div>
