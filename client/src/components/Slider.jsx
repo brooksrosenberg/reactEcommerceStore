20b8a7a4e0bf0a5f213d3c069b75485d8e3ba803
Colin Mosi Cargile <cargile10@gmail.com>
2022-04-02 15:13:18 -0400

    changed the route for the home page men's collection feature section button

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './slider.css'
 import Vans from '../img/pexels-garrett-morrow-702350.jpg'
+import { Link } from 'react-router-dom' 
 
 
 export const Slider = () => {
@@ -13,7 +14,9 @@ export const Slider = () => {
                 <div className="info-container">
                     <h1 className='slider-title'>New Spring Mens's Collection</h1>
                     <p className='slider-description'>Checkout the new Spring Men's Collection</p>
+                    <Link to='/category/Mens'>
                     <button className='slider-button'>Go!</button>
+                    </Link>
                 </div>
             </div>
         </div>
