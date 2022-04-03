649faf4a60102775c598efa1beae7f2c064039d8
Will Neal <willwneal@gmail.com>
2022-04-02 21:09:18 -0400

    Stripe now describes and charges correct cart amount

--- client/src/components/Pay.jsx
+++ client/src/components/Pay.jsx
@@ -4,11 +4,14 @@ import './pay.css'
 import {useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
+import { useSelector } from 'react-redux'
 
 
 const KEY = 'pk_test_51Kj9WsHSZblxpMo71ICY5jiZ9YeadGVW4XfJP7SMSuTqJp0sTkICSvcinn71f7k5hRvB80r50zw99vfdx0DhJX3200QnCbwCzX' 
 
 const Pay = () => {
+    const cart = useSelector(state=>state.cart)
+    
     const [stripeToken, setStripeToken] = useState(null)
     const navigate = useNavigate()
 
@@ -19,7 +22,7 @@ const Pay = () => {
     useEffect(()=>{
         const makeRequest = async () => {
             try{
-                const res = await axios.post('http://localhost:3001/api/checkout/payment', {
+                const res = await axios.post('/api/checkout/payment', {
                     tokenId: stripeToken.id,
                     amount: 2000,
                 })
@@ -38,8 +41,8 @@ const Pay = () => {
             name="Craft"
             billingAddress
             shippingAddress
-            description='Your total is $20'
-            amount={2000}
+            description={`Your total is $${cart.total}`}
+            amount={(cart.total) * 100}
             token={onToken}
             stripeKey={KEY}
             >
