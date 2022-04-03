a8c75d699a63f2c2de4f1a56ea4efe8b20033bf5
Shalah <zoungranaala@gmail.com>
2022-04-03 13:39:14 -0400

    Fixing a bug

--- client/src/pages/Shoppingcart.jsx
+++ client/src/pages/Shoppingcart.jsx
@@ -37,6 +37,7 @@ function Shoppingcart() {
 
         <section className="cart-section-bottom">
           <p className="cart-product-info">
+            
             <div className="cart-product">
               <div className="cart-product-details">
                 <img className="cart-info-image" src="https://www.hoka.com/dw/image/v2/BDJD_PRD/on/demandware.static/-/Sites-HOKA-US-master/default/dwa8219b60/images/transparent/1110519-BFBG_1.jpg?sw=414&sfrm=png&q=0&bgcolor=FFFFFF"/>
@@ -158,113 +159,38 @@ export default Shoppingcart
   // console.log(post[0].products)
 
   // const products = post[0].products;
-<<<<<<< HEAD
+//   const token = localStorage.getItem('token')
+//   const username = localStorage.getItem('username')
+//   const submitForm = async () => {
+//     if (!token) return alert ('You are not logged in, to continue checkout please sign in.')
+//     else try {
+//       navigate('/pay');
+//     }
+//     catch(err) {console.log(err)}
+//   }
+//   const gobackForm = async () => {
+//     try {
+//       navigate('/');
+//     }
+//     catch(err) {console.log(err)}
+//   }
 
-  /////////////////////////////////////
-  /////////////////////////////////////
+//   const clearCart = () => {
+//     window.location.reload(false);
+//   };
 
-  // const submitForm = async () => {
-  //   try {
-  //     navigate('/pay');
-  //   }
-  //   catch(err) {console.log(err)}
-  // }
-  // const gobackForm = async () => {
-  //   try {
-  //     navigate('/');
-  //   }
-  //   catch(err) {console.log(err)}
-  // }
-
-  // return (
-
-  // )
-
-    // ORIGINAL DESIGN
-    
-    // <div className="cart-container">
-    //   <div className="cart-wrapper">
-    //     <div className="cart-card-container">
-    //       <h1 className="cart-title">Shopping Bag</h1>
-    //       {/* <p className="cart-description">View your items below</p> */}
-    //         <div className="cart-items">
-    //             <li className="cart-list">
-    //                 <ul>{products.map((cart) => {
-    //                   console.log(cart.title);
-    //                 return (
-    //                   <div key={cart}>
-    //                     <img className='cart-product-img' alt='cart product'src={cart.img}></img>
-    //                   <div> <span className="cart-details"> Title: </span> {cart.title}</div>
-    //                   <div> <span className="cart-details"> Description: </span> {cart.description}</div>
-    //                   <div> <span className="cart-details"> Category: </span> {cart.category}</div>
-    //                   <div> <span className="cart-details"> Size: </span> {cart.size}</div>
-    //                   <div> <span className="cart-details"> Price: $</span> {cart.price}</div>
-    //                   <div> <span className="cart-details"> Location: </span> {cart.location}</div>
-    //                   <br></br>
-    //                   <br></br>
-    //                   </div>
-    //                 )})}
-    //                 </ul>
-    //             </li>
-    //         </div>
-
-    //         <div className="cart-buttons">
-    //           <div className='checkout-button'>
-    //             <input onClick={submitForm} type='button' value='Checkout'></input>
-    //           </div>
-    //           <div className='Go-Back-button'>
-    //             <input onClick={gobackForm} type='button' value='Go Back'></input>
-    //           </div>
-    //         </div>
-           
-    //     </div>
-    //   </div>
-    // </div>
-
-    // ----------------------
-
-    
+//   return (
 //     <div className="cart-container">
 //       <div className="cart-wrapper">
-//         <div className="cart-title">
-//             <h1 >Shopping Bag</h1>
-//           </div>
 //         <div className="cart-card-container">
-          
-          
-//           {/* <p className="cart-description">View your items below</p> */}
+//           <h1 className="cart-title">{username}'s Bag</h1>
+//           <p className="cart-description">View your items below</p>
 //             <div className="cart-items">
 //                 <li className="cart-list">
 //                   {/* //changing products.map to allProducts.map to get cart items from redux instead of database */}
 //                     <ul>{cart.products.map((cart) => {
 //                       console.log(cart.title);
 //                     return (
-// <<<<<<< HEAD
-
-//                       <div key={cart}>
-
-//                       <div className="cart-details-div">
-
-//                         <div className='cart-product-img-div'>
-//                           <img className='cart-product-img' alt='cart product'src={cart.img}></img>
-//                         </div>
-
-//                         <div className="cart-info-div">
-//                           <div className="cart-details"> <span className="cart-details-info">{cart.title} </span> </div>
-//                           <br></br>
-//                           {/* <div className="cart-details"> <span> Description: </span> {cart.description}</div> */}
-//                           {/* <div className="cart-details"> <span> Category: </span> {cart.category}</div> */}
-//                           <div className="cart-details"> <span> Size: </span> {cart.size}</div>
-//                           <br></br>
-//                           <div className="cart-details"> <span> Price: $ </span> {cart.price}</div>
-//                           {/* <div className="cart-details"> <span> Location: </span> {cart.location}</div> */}
-//                           <br></br>
-//                           <br></br>
-//                         </div>
-                        
-//                       </div>
-                      
-// =======
 //                       <div key={cart.id}>
 //                       <div>Title:{cart.title}</div>
 //                       {/* <div>Description:{cart.description}</div> */}
@@ -275,161 +201,26 @@ export default Shoppingcart
 //                       <div>Price:{cart.price}</div>
 //                       <div>Total: {cart.quantity * cart.price}</div>
 //                       <div>Location:{cart.location}</div>
-// >>>>>>> main
 //                       </div>
-
+                    
 //                     )})}
 //                     </ul>
 //                 </li>
 //             </div>
-// <<<<<<< HEAD
-
-            
-           
-// =======
 //             <div className="cart-total">
 //               <h3>Total: ${cart.total}</h3></div>
 //             <div className='checkout-button'>
 //               <input onClick={submitForm} type='button' value='Checkout'></input>
 //             </div>
+//             <div className='checkout-button'>
+//               <input onClick={clearCart} type='button' value='Clear Cart'></input>
+//             </div>
 //             <div className='Go-Back-button'>
 //               <input onClick={gobackForm} type='button' value='Go Back'></input>
 //             </div>
-// >>>>>>> main
 //         </div>
-//         <div className="cart-buttons">
-//               <div className='checkout-button'>
-//                 <input onClick={submitForm} type='button' value='Checkout'></input>
-//               </div>
-//               <div className='Go-Back-button'>
-//                 <input onClick={gobackForm} type='button' value='Go Back'></input>
-//               </div>
-//             </div>
 //       </div>
 //     </div>
 //   )
-
-    //---------------------------------------------------------------
-    //---------------------------------------------------------------
-    //-----------------------
-
-
-    // <div className="cart-container">
-    //   <div className="cart-wrapper">
-    //     <div className="cart-title">
-    //         <h1 >Shopping Bag</h1>
-    //       </div>
-    //     <div className="cart-card-container">
-          
-          
-    //       {/* <p className="cart-description">View your items below</p> */}
-    //         <div className="cart-items">
-    //             <li className="cart-list">
-    //               {/* //changing products.map to allProducts.map to get cart items from redux instead of database */}
-    //                 <ul>{cart.products.map((cart) => {
-    //                   console.log(cart.title);
-    //                 return (
-    //                   <div key={cart.id}>
-    //                   <div>Title:{cart.title}</div>
-    //                   {/* <div>Description:{cart.description}</div> */}
-    //                   <img className='cart-product-img' alt='cart product'src={cart.img}></img>
-    //                   <div>Category:{cart.category}</div>
-    //                   {/* <div>Size:{cart.size}</div> */}
-    //                   <div>Quantity:{cart.quantity}</div>
-    //                   <div>Price:{cart.price}</div>
-    //                   <div>Total: {cart.quantity * cart.price}</div>
-    //                   <div>Location:{cart.location}</div>
-    //                   </div>
-
-    //                 )})}
-    //                 </ul>
-    //             </li>
-    //         </div>
-    //         <div className="cart-total">
-    //           <h3>Total: ${cart.total}</h3></div>
-    //         <div className='checkout-button'>
-    //           <input onClick={submitForm} type='button' value='Checkout'></input>
-    //         </div>
-    //         <div className='Go-Back-button'>
-    //           <input onClick={gobackForm} type='button' value='Go Back'></input>
-    //         </div>
-    //     </div>
-    //     <div className="cart-buttons">
-    //           <div className='checkout-button'>
-    //             <input onClick={submitForm} type='button' value='Checkout'></input>
-    //           </div>
-    //           <div className='Go-Back-button'>
-    //             <input onClick={gobackForm} type='button' value='Go Back'></input>
-    //           </div>
-    //         </div>
-    //   </div>
-    // </div>
-  // )
-//}
-=======
-  const token = localStorage.getItem('token')
-  const username = localStorage.getItem('username')
-  const submitForm = async () => {
-    if (!token) return alert ('You are not logged in, to continue checkout please sign in.')
-    else try {
-      navigate('/pay');
-    }
-    catch(err) {console.log(err)}
-  }
-  const gobackForm = async () => {
-    try {
-      navigate('/');
-    }
-    catch(err) {console.log(err)}
-  }
-
-  const clearCart = () => {
-    window.location.reload(false);
-  };
-
-  return (
-    <div className="cart-container">
-      <div className="cart-wrapper">
-        <div className="cart-card-container">
-          <h1 className="cart-title">{username}'s Bag</h1>
-          <p className="cart-description">View your items below</p>
-            <div className="cart-items">
-                <li className="cart-list">
-                  {/* //changing products.map to allProducts.map to get cart items from redux instead of database */}
-                    <ul>{cart.products.map((cart) => {
-                      console.log(cart.title);
-                    return (
-                      <div key={cart.id}>
-                      <div>Title:{cart.title}</div>
-                      {/* <div>Description:{cart.description}</div> */}
-                      <img className='cart-product-img' alt='cart product'src={cart.img}></img>
-                      <div>Category:{cart.category}</div>
-                      {/* <div>Size:{cart.size}</div> */}
-                      <div>Quantity:{cart.quantity}</div>
-                      <div>Price:{cart.price}</div>
-                      <div>Total: {cart.quantity * cart.price}</div>
-                      <div>Location:{cart.location}</div>
-                      </div>
-                    
-                    )})}
-                    </ul>
-                </li>
-            </div>
-            <div className="cart-total">
-              <h3>Total: ${cart.total}</h3></div>
-            <div className='checkout-button'>
-              <input onClick={submitForm} type='button' value='Checkout'></input>
-            </div>
-            <div className='checkout-button'>
-              <input onClick={clearCart} type='button' value='Clear Cart'></input>
-            </div>
-            <div className='Go-Back-button'>
-              <input onClick={gobackForm} type='button' value='Go Back'></input>
-            </div>
-        </div>
-      </div>
-    </div>
-  )
-}
->>>>>>> main
+// }
 
