3e564c1b3528addab8ff95ac2493c3c02820a87c
Will Neal <willwneal@gmail.com>
2022-04-03 12:46:42 -0400

    Created working contact page along with route

---
+++ client/src/components/Contact.jsx
@@ -0,0 +1,41 @@
+import './contact.css'
+import React from 'react'
+import { useRef, useState } from 'react'
+import emailjs from 'emailjs-com'
+
+const Contact = () => {
+    //emailjs function
+    //Importin form reference from react and passing it to the form tag as a reference
+    const formRef = useRef()
+    //use State
+    const [done, setDone] = useState(false)
+    //function to handle form submission
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        emailjs.sendForm('service_eea4bqg', 'template_57fcrbv', formRef.current, 'y2-uDLRs5AvWjVBRW')
+            .then((result) => {
+                console.log(result.text);
+                setDone(true)
+            },
+                (error) => {
+                    console.log(error.text);
+                })
+    }
+    return (
+        <div className='c-container'>
+            <p className="c-desc">If you would like to be featured on this site, please use this form to contact us</p>
+            <form ref={formRef} onSubmit={handleSubmit}>
+                <div className='form-container'>
+                <input type="text" placeholder='Name' name="user_name" />
+                <input type="text" placeholder='Subject' name="user_subject" />
+                <input type="text" placeholder='Email' name="user_email" />
+                <textarea rows="5" placeholder='Message' name="message" />
+                </div>
+                <button>submit</button>
+                {done && <div className='thanks'>Thank you!</div>}
+            </form>
+        </div>
+    )
+}
+
+export default Contact
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import '../components/Footer.css';
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
+    const token = localStorage.getItem('token')
+
     return ( 
         <footer className='footer-body'>
             <div className='footer-container'>
                 <div className='footer-row2'>
                     <p className='footer-col-sm'>
-                        Copyright &copy;{new Date().getFullYear()} LIMITLESS REEFER INC | All Rights Reserved | Terms of Service | Privacy
+                        Copyright &copy;{new Date().getFullYear()} LIMITLESS REEFER INC | All Rights Reserved | Terms of Service | Privacy {token && <Link className='contact-link' to="/contact">| Contact</Link>}
                     </p>
                 </div>
             </div>
